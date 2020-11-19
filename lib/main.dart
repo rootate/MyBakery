3feb6228bccd1566642c8e0a6f64d2d5b94e0cd0
harunalbayrak <harunnalbayrakk278@gmail.com>
2020-11-20 00:08:16 +0300

    Add provider class & wrapper page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'screens/home/home.dart';
-import 'screens/authenticate/authenticate.dart';
+import 'package:flutter_my_bakery/models/user.dart';
+import 'package:flutter_my_bakery/screens/wrapper.dart';
+import 'package:flutter_my_bakery/services/auth.dart';
+import 'package:provider/provider.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -9,15 +12,11 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
       ),
-      home: Authenticate(),
     );
   }
 }
-
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_my_bakery/models/user.dart';
+import 'package:flutter_my_bakery/screens/authenticate/authenticate.dart';
+import 'package:flutter_my_bakery/screens/home/home.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+
+    // return either Home or Authenticate widget
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
+  }
+}
