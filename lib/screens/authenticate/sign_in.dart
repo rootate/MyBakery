99264a6e9a1c5dfa941d3ba3c808b51264ba7277
harunalbayrak <harunnalbayrakk278@gmail.com>
2020-11-19 23:00:01 +0300

    Add sign_in page

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_my_bakery/services/auth.dart';
+import 'package:flutter_my_bakery/shared/constants.dart';
+import 'package:flutter_my_bakery/shared/loading.dart';
 
 class SignIn extends StatefulWidget {
   final Function toggleView;
@@ -9,8 +12,85 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
+
+  String email = "";
+  String password = "";
+  String error = "";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return loading ? Loading() : Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text("Sign In"),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text("Register"),
+            onPressed: () {
+              widget.toggleView();
+            },
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0,),
+              TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: "Email"),
+                validator: (val) => val.isEmpty ? "Enter an email" : null,
+                onChanged: (val) {
+                  setState(() {
+                    return email = val;
+                  });
+                },
+              ),
+              SizedBox(height: 20.0,),
+              TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: "Password"),
+                validator: (val) => val.length < 6 ? "Enter a password 6+ chars long" : null,
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() {
+                    return password = val;
+                  });
+                },
+              ),
+              SizedBox(height: 20.0,),
+              RaisedButton(
+                color: Colors.pink[400],
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async{
+                  if(_formKey.currentState.validate()){
+                    setState(() => loading = true);
+                    dynamic result = await _auth.signInWithEmailAndPassword(email,password);
+                    if(result == null){
+                      setState(() {
+                        loading = false;
+                        return error = "Could not sign in with those credentials";
+                      });
+                    }
+                  }
+                },
+              ),
+              SizedBox(height: 20.0),
+              Text(error,style: TextStyle(color: Colors.red,fontSize: 14.0),)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
