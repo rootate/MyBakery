e00b6bfd7d454b907aa7d961441a8a9d8586d898
harunalbayrak <harunnalbayrakk278@gmail.com>
2020-11-19 22:43:06 +0300

    Add AuthService class

---
+++ lib/services/auth.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create User obj based on firebaseUser
+
+  // auth change user stream
+
+  // sign in with email&password
+
+  // register with email&password
+
+  // sign out
+
+}
