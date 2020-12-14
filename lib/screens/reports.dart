3123454481f9048bb974a29c4ba8be33ad50e928
harunalbayrak <harunnalbayrakk278@gmail.com>
2020-12-14 23:24:01 +0300

    Fix datepicker

--- lib/screens/reports.dart
+++ lib/screens/reports.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:flutter_my_bakery/shared/constants.dart';
 import 'package:flutter_my_bakery/shared/bottom_bar.dart';
+import 'package:intl/intl.dart';
 
 class Reports extends StatefulWidget {
   @override
@@ -20,12 +21,28 @@ class _ReportsState extends State<Reports> {
   @override
   Widget build(BuildContext context) {
     double size1 = MediaQuery.of(context).size.height / 50;
-    double size2 = MediaQuery.of(context).size.height / 35;
+    double size2 = MediaQuery.of(context).size.height / 10;
+    double size3 = MediaQuery.of(context).size.height / 20;
+
+    int toplamCikanEkmek = 1200;
+    int dagitimdaSatilanEkmek = 100;
+    int toplamKalanEkmek = 1000;
+    int veresiyeSatilanEkmek = 100;
+
+    int vitrindenToplamSatisTutari = 300;
+    int krediKartiSatisTutari = 400;
+    int kasadaOlmasiGerekenTutar = 500;
+
+    DateFormat dateFormat = DateFormat("dd.MM.yyyy");
+    String currentTime = dateFormat.format(DateTime.now());
+    DateTime minTime = DateTime(2018, 12, 5);
+    DateTime maxTime = DateTime.now();
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Reports",style: TextStyle(fontFamily: "Poppins"),),
         centerTitle: true,
+        backgroundColor: Colors.blueGrey,
       ),
       body: LayoutBuilder(builder: (context,constraints){
         return SingleChildScrollView(
@@ -51,7 +68,7 @@ class _ReportsState extends State<Reports> {
                                 child: Row(
                                   mainAxisSize: MainAxisSize.min,
                                   children: [
-                                    Text("4 March 2020",style: TextStyle(fontFamily: "Poppins",color: Colors.white),),
+                                    Text(currentTime,style: TextStyle(fontFamily: "Poppins",color: Colors.white),),
                                     Icon(Icons.keyboard_arrow_right ,color: Colors.white,),
                                   ],
                                 ),
@@ -59,8 +76,8 @@ class _ReportsState extends State<Reports> {
                                 onPressed: () {
                                   DatePicker.showDatePicker(context,
                                   showTitleActions: true,
-                                  minTime: DateTime(2018, 12, 5),
-                                  maxTime: DateTime(2020, 12, 14), onChanged: (date) {
+                                  minTime: minTime,
+                                  maxTime: maxTime, onChanged: (date) {
                                     print('change $date');
                                   }, onConfirm: (date) {
                                     print('confirm $date');
@@ -70,21 +87,21 @@ class _ReportsState extends State<Reports> {
                               Column(
                                 children: [
                                   Text("Vitrinden toplam satış tutarı",style: textStyle(Colors.green),),
-                                  Text("₺ 300", style: textStyle(Colors.green),),
+                                  Text("₺ " + vitrindenToplamSatisTutari.toString(), style: textStyle(Colors.green),),
                                 ],
                               ),
                               SizedBox(height: size1,),
                               Column(
                                 children: [
                                   Text("Kredi kartı ile yapılan satış tutarı",style: textStyle(Colors.purple),),
-                                  Text("₺ 300", style: textStyle(Colors.purple),),
+                                  Text("₺ " + krediKartiSatisTutari.toString(), style: textStyle(Colors.purple),),
                                 ],
                               ),
                               SizedBox(height: size1,),
                               Column(
                                 children: [
                                   Text("Kasada olması gereken tutar",style: textStyle(Colors.blue),),
-                                  Text("₺ 300", style: textStyle(Colors.blue),),
+                                  Text("₺ " + kasadaOlmasiGerekenTutar.toString(), style: textStyle(Colors.blue),),
                                 ],
                               ),
                             ],
@@ -92,7 +109,7 @@ class _ReportsState extends State<Reports> {
                         ),
                       ),
                     ),
-                    SizedBox(height: size2,),
+                    SizedBox(height: size1,),
                     Center(
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -104,10 +121,11 @@ class _ReportsState extends State<Reports> {
                               decoration: BoxDecoration(color: Color(0xff633333),borderRadius: BorderRadius.all(Radius.circular(30.0))),
                               child: Column(
                                 children: [
-                                  Image(image: AssetImage('assets/images/bread.png')),
+                                  Image(image: AssetImage('assets/images/bread.png'),width: size2,),
+                                  SizedBox(height: size1,),
                                   Text("Toplam Çıkan Ekmek",style: textStyle2,textAlign: TextAlign.center,),
                                   SizedBox(height: size1,),
-                                  Text("1200",style: textStyle3,textAlign: TextAlign.center,)
+                                  Text(toplamCikanEkmek.toString() ,style: textStyle3,textAlign: TextAlign.center,)
                                 ],
                               ),
                             ),
@@ -120,10 +138,11 @@ class _ReportsState extends State<Reports> {
                               decoration: BoxDecoration(color: Color(0xff2b516c),borderRadius: BorderRadius.all(Radius.circular(30.0))),
                               child: Column(
                                 children: [
-                                  Image(image: AssetImage('assets/images/bread.png')),
+                                  Image(image: AssetImage('assets/images/bread.png'),width: size2,),
+                                  SizedBox(height: size1,),
                                   Text("Dağıtımda Satılan Ekmek",style: textStyle2,textAlign: TextAlign.center,),
-                                  SizedBox(height: size1),
-                                  Text("1200",style: textStyle3,textAlign: TextAlign.center,)
+                                  SizedBox(height: size1,),
+                                  Text(dagitimdaSatilanEkmek.toString() ,style: textStyle3,textAlign: TextAlign.center,)
                                 ],
                               ),
                             ),
@@ -136,17 +155,35 @@ class _ReportsState extends State<Reports> {
                               decoration: BoxDecoration(color: Color(0xff773774),borderRadius: BorderRadius.all(Radius.circular(30.0))),
                               child: Column(
                                 children: [
-                                  Image(image: AssetImage('assets/images/bread.png')),
+                                  Image(image: AssetImage('assets/images/bread.png'), width: size2,),
+                                  SizedBox(height: size1,),
                                   Text("Toplam Kalan Ekmek",style:  textStyle2,textAlign: TextAlign.center,),
                                   SizedBox(height: size1,),
-                                  Text("1200",style: textStyle3,textAlign: TextAlign.center,)
+                                  Text(toplamKalanEkmek.toString() ,style: textStyle3,textAlign: TextAlign.center,)
                                 ],
                               ),
                             ),
                           ),
                         ],
                       ),
-                    )
+                    ),
+                    SizedBox(height: size1,),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width - 20,
+                      height: MediaQuery.of(context).size.height / 10,
+                      child: Container(
+                        decoration: BoxDecoration(color: Color(0xff31402c),borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                        child: Row(
+                          children: [
+                            Image(image: AssetImage('assets/images/bread.png'),width: size2,),
+                            SizedBox(width: size3 - 10,),
+                            Text("Veresiye Satılan Ekmek",style: textStyle2,textAlign: TextAlign.center,),
+                            SizedBox(width: size3,),
+                            Text(veresiyeSatilanEkmek.toString() ,style: textStyle3,textAlign: TextAlign.center,)
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               )
