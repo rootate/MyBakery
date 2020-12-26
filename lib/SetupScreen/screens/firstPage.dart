bb46eeb905c057d2827523fb8a56e17ae3205f12
Omer Faruk <omerfsayar@gmail.com>
2020-12-26 03:44:05 +0300

    First page screen create for setup screen

---
+++ lib/SetupScreen/screens/firstPage.dart
@@ -0,0 +1,65 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_my_bakery/shared/constants.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            "assets/images/setupbg.jpeg",
+            fit: BoxFit.cover,
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+            child: Column(
+              children: [
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    color: Colors.white70,
+                    child: ListTile(
+                      title: Container(
+                        margin: EdgeInsets.only(bottom: 10, top: 5),
+                        child: Text(
+                          "HOŞGELDİNİZ",
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      subtitle: Text(
+                        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 16,
+                          wordSpacing: 0.75,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment(0.6, 0),
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text("BAŞLAYALIM"),
+                    color: Colors.orange[700],
+                  ),
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
