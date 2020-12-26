6b48e7b45954ca9b33f803f6aac0b107b3cb560a
Omer Faruk <omerfsayar@gmail.com>
2020-12-27 02:43:06 +0300

    adding product to categories future implement

---
+++ lib/SetupScreen/models/Product.dart
@@ -0,0 +1,6 @@
+class Product {
+  final String name;
+  final double amount;
+
+  const Product({this.name, this.amount});
+}
--- lib/SetupScreen/widgets/categoryItem.dart
+++ lib/SetupScreen/widgets/categoryItem.dart
@@ -1,32 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_my_bakery/SetupScreen/screens/urunler.dart';
+import '../models/Product.dart';
 
 class CategoryItem extends StatelessWidget {
   final String name;
   final String image;
+  final List<Product> liste = [];
 
   CategoryItem(this.name, this.image);
 
+  void goProduct(BuildContext cx) {
+    Navigator.of(cx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return Urunler(
+            category: name,
+            list: liste,
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 7,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            margin: EdgeInsets.only(bottom: 14),
-            child: Image.asset(
-              image,
-              height: MediaQuery.of(context).size.height * 0.1,
+    return InkWell(
+      onTap: () => goProduct(context),
+      child: Card(
+        elevation: 7,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 14),
+              child: Image.asset(
+                image,
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
             ),
-          ),
-          Text(
-            name,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          )
-        ],
+            Text(
+              name,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
