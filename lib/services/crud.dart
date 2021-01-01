1c735f7178d238910f769a04cbbca8feb777afe6
Bilal Bayrakdar <bilallbayrakdar@gmail.com>
2021-01-01 17:39:49 +0300

    Update crud.dart
    Co-Authored-By: yusufaakgul <62053682+yusufaakgul@users.noreply.github.com>

--- lib/services/crud.dart
+++ lib/services/crud.dart
@@ -1,14 +1,24 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
 
 class DatabaseService {
+  final DateFormat formatter = DateFormat('yyyy-MM-dd'); 
   var uuid = Uuid();
   final employeesReference =
       FirebaseDatabase.instance.reference().child('bakery').child('employees');
 
+  final producedBreadReference =
+      FirebaseDatabase.instance.reference().child('bakery').child('dailyData');
+
   void addEmployee(Map data) {
     var v1 = uuid.v1();
     employeesReference.child(v1).set(data);
+  }
+
+  void addEkmek(Map data) {
+    var day = DateTime.now();
   }
 
   void updateEmployee(String uid, Map data) {
