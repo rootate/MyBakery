b15b2439f985a76c270057b147875aa9b4b129a8
Omer Faruk <omerfsayar@gmail.com>
2021-01-03 23:23:08 +0300

    styling

--- lib/widgets/categoryItem.dart
+++ lib/widgets/categoryItem.dart
@@ -26,25 +26,29 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () => goProduct(context),
-      child: Card(
-        elevation: 7,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              margin: EdgeInsets.only(bottom: 14),
-              child: Image.asset(
-                image,
-                height: MediaQuery.of(context).size.height * 0.11,
+      child: Container(
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          elevation: 7,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 14),
+                child: Image.asset(
+                  image,
+                  height: MediaQuery.of(context).size.height * 0.11,
+                ),
               ),
-            ),
-            Text(
-              name,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            )
-          ],
+              Text(
+                name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
