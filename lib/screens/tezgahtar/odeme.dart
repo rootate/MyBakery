bb760506ebeba02a6649487f1e3282c68ea1accf
Bilal Bayrakdar <bilallbayrakdar@gmail.com>
2021-01-31 14:15:28 +0300

    Update odeme.dart
    Co-Authored-By: yusufaakgul <62053682+yusufaakgul@users.noreply.github.com>

--- lib/screens/tezgahtar/odeme.dart
+++ lib/screens/tezgahtar/odeme.dart
@@ -3,8 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_my_bakery/screens/tezgahtar/odeme_kategori.dart';
 import 'package:flutter_my_bakery/services/databaseService.dart';
 
-List images = ['ekmekler.jpeg','kahvaltiliklar.jpeg','pastalar.jpeg','icecekler.jpeg',
-  'tatlilar.jpeg','kurabiyeler.jpeg','hazirGidalar.jpeg','diger.jpeg','diger.jpeg','diger.jpeg','diger.jpeg','diger.jpeg','diger.jpeg'];
+List images = [
+  'ekmekler.jpeg',
+  'kahvaltiliklar.jpeg',
+  'pastalar.jpeg',
+  'icecekler.jpeg',
+  'tatlilar.jpeg',
+  'kurabiyeler.jpeg',
+  'hazirGidalar.jpeg',
+  'diger.jpeg',
+  'diger.jpeg',
+  'diger.jpeg',
+  'diger.jpeg',
+  'diger.jpeg',
+  'diger.jpeg'
+];
 
 class Odeme extends StatefulWidget {
   @override
@@ -28,79 +41,88 @@ class _OdemeState extends State<Odeme> {
 
     return StreamBuilder<Event>(
       stream: service.categoryReference.onValue,
-      builder: (context,snapshot){
+      builder: (context, snapshot) {
         Map data = {};
         List item = [];
-        if(snapshot.hasData) {
+        if (snapshot.hasData) {
           data = snapshot.data.snapshot.value;
-          if(data == null){
+          if (data == null) {
             return Scaffold(
               appBar: AppBar(
-                title: Text("Ödeme",style: TextStyle(fontFamily: "Poppins"),),
+                title: Text(
+                  "Kategoriler",
+                  style: TextStyle(fontFamily: "Poppins"),
+                ),
                 centerTitle: true,
                 backgroundColor: Colors.blueGrey,
               ),
             );
           }
-          data.forEach(
-                  (index, data) => item.add({"key": index, ...data}));
+          data.forEach((index, data) => item.add({"key": index, ...data}));
         }
 
-        if (snapshot.hasError)
-          return new Text('Error: ${snapshot.error}');
-        switch (snapshot.connectionState){
+        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+        switch (snapshot.connectionState) {
           case ConnectionState.waiting:
-            return  Container(
+            return Container(
               height: 200.0,
               alignment: Alignment.center,
               child: CircularProgressIndicator(
                 valueColor: AlwaysStoppedAnimation<Color>(Colors.black45),
               ),
             );
-          default: return Scaffold(
-            appBar: AppBar(
-              title: Text("Ödeme",style: TextStyle(fontFamily: "Poppins"),),
-              centerTitle: true,
-              backgroundColor: Colors.blueGrey,
-            ),
-            body: ListView.builder(
-              itemCount: item.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  // onLongPress: (){
-                  //   controller.text = categories[index];
-                  //   confirmationPopup(context,image,1,index,controller);
-                  // },
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => OdemeKategori(category: item[index]["key"])),
-                    );
-                  },
-                  leading: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      minWidth: sizeW,
-                      minHeight: sizeH,
-                      maxWidth: sizeW + 20,
-                      maxHeight: sizeH + 20,
+          default:
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(
+                  "Ödeme",
+                  style: TextStyle(fontFamily: "Poppins"),
+                ),
+                centerTitle: true,
+                backgroundColor: Colors.blueGrey,
+              ),
+              body: ListView.builder(
+                itemCount: item.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    // onLongPress: (){
+                    //   controller.text = categories[index];
+                    //   confirmationPopup(context,image,1,index,controller);
+                    // },
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                OdemeKategori(category: item[index]["key"])),
+                      );
+                    },
+                    leading: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        minWidth: sizeW,
+                        minHeight: sizeH,
+                        maxWidth: sizeW + 20,
+                        maxHeight: sizeH + 20,
+                      ),
+                      child: Image(
+                          image: AssetImage('assets/images/' + images[index])),
                     ),
-                    child: Image(image: AssetImage('assets/images/' + images[index])),
-                  ),
-                  title: Text(item[index]["key"],style: TextStyle(fontFamily: "Poppins"),),
-                );
-              },
-
-            ),
-            // floatingActionButton: FloatingActionButton(
-            //   onPressed: (){
-            //     confirmationPopup(context,image,0,0,controller);
-            //   },
-            //   // child: Icon(Icons.add),
-            // ),
-          );
+                    title: Text(
+                      item[index]["key"],
+                      style: TextStyle(fontFamily: "Poppins"),
+                    ),
+                  );
+                },
+              ),
+              // floatingActionButton: FloatingActionButton(
+              //   onPressed: (){
+              //     confirmationPopup(context,image,0,0,controller);
+              //   },
+              //   // child: Icon(Icons.add),
+              // ),
+            );
         }
       },
     );
   }
 }
-
